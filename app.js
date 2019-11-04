b9d9d7e9810a66d2790be17a50593c8bad030f86
MAGDX <magnum_dx@hotmail.com>
2019-11-04 09:09:50 +0100

    Initial import...

---
+++ app.js
@@ -0,0 +1,3 @@
+var Visualizer = window["ui-router-visualizer"].Visualizer;
+var app = angular.module("app", ["ui.router"]);
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,2 @@
+let Visualizer = window["ui-router-visualizer"].Visualizer;
+const app = angular.module("app", ["ui.router"]);
